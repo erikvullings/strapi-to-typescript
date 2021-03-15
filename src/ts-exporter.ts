647b0a8d7cce6f04254b01397789f42e60984482
Guðmundur Stefán Þorvaldsson <gummis@gmail.com>
2021-03-15 15:34:24 +0000

    refactor: use config function

--- src/ts-exporter.ts
+++ src/ts-exporter.ts
@@ -210,7 +210,7 @@ class Converter {
     const toImportDefinition = (name: string) => {
       const found = findModel(this.strapiModels, name);
       const toFolder = (f: IStrapiModelExtended) => (this.config.nested ? `../${f.snakeName}/${f.snakeName}` : `./${f.snakeName}`);
-      return found ? `import ${(this.config.importAsType ? 'type ': '')}{ ${found.interfaceName} } from '${toFolder(found)}';` : '';
+      return found ? `import ${((this.config.importAsType && this.config.importAsType(m.interfaceName)) ? 'type ': '')}{ ${found.interfaceName} } from '${toFolder(found)}';` : '';
     };
 
     const imports: string[] = [];
